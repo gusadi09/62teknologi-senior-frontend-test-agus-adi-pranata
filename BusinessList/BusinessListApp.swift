9807f6d8e8a9ab92dc956831b7d17ef49508646f
Agus Adi <gusadi872@gmail.com>
2022-12-30 17:55:24 +0700

    Initial Commit

---
+++ BusinessList/BusinessListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessListApp.swift
+//  BusinessList
+//
+//  Created by Gus Adi on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BusinessListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
