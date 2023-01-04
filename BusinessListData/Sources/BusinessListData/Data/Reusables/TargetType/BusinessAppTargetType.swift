4c0688f492b8dcbb961127681e85a40d8ca2ce72
Agus Adi <gusadi872@gmail.com>
2023-01-04 18:31:56 +0700

    [iOS] add templated target type

---
+++ BusinessListData/Sources/BusinessListData/Data/Reusables/TargetType/BusinessAppTargetType.swift
@@ -0,0 +1,35 @@
+//
+//  BusinessAppTargetType.swift
+//  
+//
+//  Created by Gus Adi on 04/01/23.
+//
+
+import Foundation
+import Moya
+
+protocol BusinessAppTargetType: TargetType {
+	var parameters: [String: Any] {
+		get
+	}
+}
+
+extension BusinessAppTargetType {
+	var baseURL: URL {
+		URL(
+			string: "https://api.yelp.com/v3"
+		) ?? (NSURL() as URL)
+	}
+
+	var parameterEncoding: Moya.ParameterEncoding {
+		JSONEncoding.default
+	}
+
+	var task: Task {
+		return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+	}
+
+	var headers: [String : String]? {
+		return [:]
+	}
+}
